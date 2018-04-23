da16ca0ddde53f6c2827979454aad854158b6f97
Leo Toikka <leo@seravo.fi>
2018-04-23 10:23:56 +0300

    Specify port 80 and improve inline comments in smoke test

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# A simple HTTP request to the HTTP port 80 will determine, if the application is
-# running properly
-curl -sSf $1 >> /dev/null
+# A simple HTTP request to the HTTP port 80 will determine whether the application
+# is running at all in the desired IP
+curl -sSf $1:80 >> /dev/null
