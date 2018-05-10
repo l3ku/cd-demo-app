l3ku/cd-demo-app

commit 2a631c109413f81bcf2f4b4a631d8fd4b4c8e1f5

Deliberately introduce typo to verify fail of automated acceptance tests

app/index.js
@@ -33,7 +33,7 @@ function writeHTTPContent(request, response) {
     writeHead(response);
     response.write('<h1 class="page-title">Continuous Delivery Demo Application</h1>');
     response.write('<p class="page-description">This is a demo app that I use in the Continuous Delivery pipeline in my Bachelor\'s thesis.</p>');
-    response.write('<small class="page-ps">P.S. Check the <a class="github-link" href="https://github.com/l3ku/cd-demo-app">GitHub repo</a> for details</small>');
+    response.write('<small class="page-ps">P.S. Check <a class="github-link" href="https://github.com/l3ku/cd-demo-app">GitHub repo</a> for details</small>');
     writeFooter(response);
     response.end();
 }

commit c97c6883071a6624ce64e7da669de36b41b90925

Fix bug introduced in 2a631c1 that caused automatic acceptance test failure

app/index.js
@@ -33,7 +33,7 @@ function writeHTTPContent(request, response) {
     writeHead(response);
     response.write('<h1 class="page-title">Continuous Delivery Demo Application</h1>');
     response.write('<p class="page-description">This is a demo app that I use in the Continuous Delivery pipeline in my Bachelor\'s thesis.</p>');
-    response.write('<small class="page-ps">P.S. Check <a class="github-link" href="https://github.com/l3ku/cd-demo-app">GitHub repo</a> for details</small>');
+    response.write('<small class="page-ps">P.S. Check the <a class="github-link" href="https://github.com/l3ku/cd-demo-app">GitHub repo</a> for details</small>');
     writeFooter(response);
     response.end();
 }